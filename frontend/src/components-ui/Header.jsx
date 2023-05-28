79c9971d92dad5008e2e9ce1ce70670e456a5fdd
badrsh980 <Badrsh980@gmail.com>
2023-05-28 20:24:49 +0300

    up data

---
+++ frontend/src/components-ui/Header.jsx
@@ -0,0 +1,16 @@
+import { Navbar, nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, faUser } from 'react-icons/fa';
+
+
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+
+      <NavBar bg="dark" variant="dark" collapseOnSelect>
+        <p></p>
+      </NavBar>
+    </header>
+  )
+}
